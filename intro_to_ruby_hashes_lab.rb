cc9d2c3a68482e16aeba3aabdc68e793dc2346f0
Brett Saxon <brettcsaxon@gmail.com>
2019-08-17 16:39:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  key = 0
-  Hash.new { |hash, key| hash[key] =  }
-  # return the correct value using the hash and key parameters
+  hash [key]
 end
 
 def update_counting_hash(hash, key)
